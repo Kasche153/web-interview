b991c0003bcc1b1d5187e9868a83274d10d2b1ce
Kasche153 <57144436+Kasche153@users.noreply.github.com>
2025-02-18 08:28:44 +0100

    working example 3/4

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import { addTodo, updateComplete, deleteItem, getTodos, updateTitle } from './db.js'
+
 
 const app = express()
 
@@ -8,6 +10,43 @@ app.use(express.json())
 
 const PORT = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/:userId', async (req, res) => {
+    const userId = req.params.userId;
+    const todos = await getTodos(userId);
+    res.send(todos)
+})
+
+
+app.post("/todo", async (req, res) => {
+    console.log(req.body)
+    const { userId, todoTitle, todoListId, todoId } = req.body;
+    addTodo(userId, todoListId, todoTitle, todoId);
+    res.send(":)")
+
+})
+
+app.put("/todoCompleteUpdate", async (req, res) => {
+    const { todoId, complete } = req.body;
+
+    updateComplete(todoId, complete);
+    res.status(200).send("")
+
+})
+
+app.put("/todoTitleUpdate", async (req, res) => {
+    const { todoId, todoTitle } = req.body;
+    console
+    updateTitle(todoId, todoTitle);
+    res.status(200).send("")
+
+})
+
+app.delete("/todo", async (req, res) => {
+    const { todoId } = req.body;
+    deleteItem(todoId);
+    res.status(200).send("")
+
+})
+
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
--- frontend/src/todos/components/TodoLists.jsx
+++ frontend/src/todos/components/TodoLists.jsx
@@ -6,38 +6,40 @@ import {
   ListItemButton,
   ListItemText,
   ListItemIcon,
+  CircularProgress,
   Typography,
 } from '@mui/material'
 import ReceiptIcon from '@mui/icons-material/Receipt'
 import { TodoListForm } from './TodoListForm'
+import { fetchTodos, deleteTodo, addTodo, putTodoTitle, putTodoComplete } from '../../API/API'
+import { v4 as uuidv4 } from 'uuid';
+import useDebouncer from '../../hooks/useDebouncer'
 
-// Simulate network
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
-const fetchTodoLists = () => {
-  return sleep(1000).then(() =>
-    Promise.resolve({
-      '0000000001': {
-        id: '0000000001',
-        title: 'First List',
-        todos: ['First todo of first list!'],
-      },
-      '0000000002': {
-        id: '0000000002',
-        title: 'Second List',
-        todos: ['First todo of second list!'],
-      },
-    })
-  )
-}
+
 
 export const TodoLists = ({ style }) => {
   const [todoLists, setTodoLists] = useState({})
   const [activeList, setActiveList] = useState()
+  const [todos, setTodos] = useState([])
+  const [updatedTodo, setUpdateTodo] = useState(null);
+  const debouncedTodo = useDebouncer(updatedTodo, 300)
+  const getTodoLists = async () => {
+    const todoList = await fetchTodos("Gustav");
+    setTodoLists(todoList);
+  }
+  useEffect(() => {
+    getTodoLists();
+  }, [])
 
   useEffect(() => {
-    fetchTodoLists().then(setTodoLists)
-  }, [])
+    if (debouncedTodo) {
+
+      putTodoTitle(debouncedTodo.todoId, debouncedTodo.todoTitle)
+    }
+  }, [debouncedTodo])
+
+
 
   if (!Object.keys(todoLists).length) return null
   return (
@@ -47,27 +49,78 @@ export const TodoLists = ({ style }) => {
           <Typography component='h2'>My Todo Lists</Typography>
           <List>
             {Object.keys(todoLists).map((key) => (
-              <ListItemButton key={key} onClick={() => setActiveList(key)}>
-                <ListItemIcon>
-                  <ReceiptIcon />
-                </ListItemIcon>
-                <ListItemText primary={todoLists[key].title} />
-              </ListItemButton>
+              <div style={{ display: "flex" }}>
+
+                <ListItemButton key={key} onClick={() => {
+                  setActiveList(key)
+                  setTodos(todoLists[key])
+                }
+                }>
+                  <ListItemIcon>
+                    <ReceiptIcon />
+                  </ListItemIcon>
+
+                  <ListItemText primary={key} />
+                </ListItemButton>
+                <CircularProgress variant="determinate" value={(todoLists[key].filter(todo => todo.complete).length / todoLists[key].length) * 100} />
+
+              </div>
             ))}
           </List>
         </CardContent>
       </Card>
       {todoLists[activeList] && (
         <TodoListForm
-          key={activeList} // use key to make React recreate component to reset internal state
-          todoList={todoLists[activeList]}
-          saveTodoList={(id, { todos }) => {
-            const listToUpdate = todoLists[id]
-            setTodoLists({
-              ...todoLists,
-              [id]: { ...listToUpdate, todos },
-            })
+          onChange={async (value, todo, index) => {
+            const newTodo = { ...todo, todoTitle: value };
+
+            setTodos((prevTodos) =>
+              prevTodos.map((todo, todoIndex) => (todoIndex === index ? newTodo : todo))
+            );
+
+            setTodoLists((prevTodoLists) => ({
+              ...prevTodoLists,
+              [activeList]: prevTodoLists[activeList].map((todo, todoIndex) =>
+                todoIndex === index ? newTodo : todo
+              ),
+            }));
+            setUpdateTodo(newTodo);
           }}
+          onDelete={(todoId, index) => {
+            const updatedTodos = todos.filter((todo) => todo.todoId !== todoId)
+            const updatedTodoLists = { ...todoLists };
+            updatedTodoLists[activeList] = updatedTodos;
+            setTodoLists(updatedTodoLists);
+            setTodos(updatedTodos);
+            deleteTodo(todoId);
+          }}
+          onAddTodo={async () => {
+            const newTodo = { "todoListId": activeList, "todoId": uuidv4() };
+            const updatedTodos = [...todos, newTodo]
+            const updatedTodoLists = { ...todoLists };
+            updatedTodoLists[activeList] = updatedTodos;
+            setTodoLists(updatedTodoLists)
+            setTodos(updatedTodos)
+            await addTodo("Gustav", newTodo.todoListId, '', newTodo.todoId);
+          }}
+          onCheck={(value, todo, index) => {
+            console.log(value)
+            const newTodo = { ...todo, complete: value };
+            setTodos((prevTodos) =>
+              prevTodos.map((todo, todoIndex) => (todoIndex === index ? newTodo : todo))
+            );
+
+            setTodoLists((prevTodoLists) => ({
+              ...prevTodoLists,
+              [activeList]: prevTodoLists[activeList].map((todo, todoIndex) =>
+                todoIndex === index ? newTodo : todo
+              ),
+            }));
+            putTodoComplete(todo.todoId, value);
+
+          }}
+          todos={todos}
+          todoListId={activeList}
         />
       )}
     </Fragment>
